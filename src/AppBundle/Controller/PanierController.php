e4fdcb527b5dff2f90bbd1f1f7f35927ea493351
MaAlexandre <masse.alexandre@hotmail.fr>
2015-05-19 14:24:37 +0200

    sdryftkl

--- src/AppBundle/Controller/PanierController.php
+++ src/AppBundle/Controller/PanierController.php
@@ -3,27 +3,34 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
 
 class PanierController extends Controller
 {
      /**
      * @Route("/panier", name="panier")
      */
-    public function listBdAction()
+    public function panierAction()
     {
         return $this->render('default/panier.html.twig');
     }
+
+    public function panier(Request $request)
+    {
+        
+    }
 }
 
-    // {% for comment in story.comments %}    
-    //         <tr>
-    //             <article class="article_panier">
-    //                 <img alt="article" src="#" class="image_article"/>
-    //                 <p class="titre_article">Titre de la bédé 1</p>
-    //                 <button class="glyphicon glyphicon-remove-circle bt_suppr"></button>
-    //                 <p class="sup_article">Retirer du panier</p>
-    //             </article>
-    //         </tr>
-    //         <button id="bt_valider">Valider ce panier</button>
-    //         <button id="bt_back">Continuer à bouquiner</button>
-    // // {% endfor %} 
+        // {% for comment in story.comments %}    
+        //     <tr>
+        //         <article class="article_panier">
+        //         <img alt="article" src="#" class="image_article"/>
+        //         <p class="titre_article">Titre de la bédé 1</p>
+        //         <button class="glyphicon glyphicon-remove-circle bt_suppr"></button>
+        //         <p class="sup_article">Retirer du panier</p>
+        //         </article>
+        //     </tr>
+            
+        // {% endfor %} 
