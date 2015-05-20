3bb0e9893587d191203312b59cf88db6fffc0ef2
MaAlexandre <masse.alexandre@hotmail.fr>
2015-05-20 11:01:20 +0200

    jusryk

--- src/AppBundle/DataFixtures/ORM/LoadBookData.php
+++ src/AppBundle/DataFixtures/ORM/LoadBookData.php
@@ -11,363 +11,263 @@ use AppBundle\Entity\Author;
 use AppBundle\Entity\Serie;
 use DateTime;
 
-// class LoadBookData implements FixtureInterface, ContainerAwareInterface
-// {
 
-//     private $pathToData;
+class LoadBookData implements FixtureInterface, ContainerAwareInterface 
+{
 
-//     private $authorRepo;
-//     private $serieRepo;
-//     private $authors = array();
-//     private $series = array();
+    private $pathToData;
 
-//     public function __construct(){
-//         $this->pathToData = __DIR__ . "/data/";
-//     }
+    private $authorRepo;
+    private $serieRepo;
+    private $authors = array();
+    private $series = array();
 
-//     /**
-//      * @var ContainerInterface
-//      */
-//     private $container;
+    public function __construct(){
+        $this->pathToData = __DIR__ . "/data/";
+    }
 
-//     /**
-//      * {@inheritDoc}
-//      */
-//     public function setContainer(ContainerInterface $container = null)
-//     {
-//         $this->container = $container;
-//     }
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
 
-//     /**
-//      * {@inheritDoc}
-//      */
-//     public function load(ObjectManager $manager)
-//     {
-//         $this->loadSeries($manager);
-//         $this->loadAuthors($manager);
-//         $this->loadBooks($manager);
-//     }
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->loadSeries($manager);
+        $this->loadAuthors($manager);
+        $this->loadBooks($manager);
+    }
 
 
-//     private function loadBooks(ObjectManager $manager){
+    private function loadBooks(ObjectManager $manager){
 
-//         $this->authorRepo = $this->container->get('doctrine')->getRepository("AppBundle:Author");
-//         $this->serieRepo = $this->container->get('doctrine')->getRepository("AppBundle:Serie");
+        $this->authorRepo = $this->container->get('doctrine')->getRepository("AppBundle:Author");
+        $this->serieRepo = $this->container->get('doctrine')->getRepository("AppBundle:Serie");
 
-//         $fh = fopen($this->pathToData."albums.csv", "r");
-//         $num = 1;
+        $fh = fopen($this->pathToData."albums.csv", "r");
+        $num = 1;
 
-//         while($row = fgetcsv($fh)){
-//             //skip first row
-//             if ($num <= 1){ 
-//                 $num++;
-//                 continue; 
-//             }
-//             /*
-//                 [0] => IdAlbum
-//                 [1] => Num
-//                 [2] => Titre
-//                 [3] => IdSerie
-//                 [4] => IdDessinateur
-//                 [5] => IdColoriste
-//                 [6] => IdScenariste
-//                 [7] => Editeur
-//                 [8] => Reference
-//                 [9] => Couverture
-//                 [10] =>
-//                 [11] => Exlibris
-//                 [12] => NbPages
-//                 [13] => Planche
-//                 [14] => IdBEL
-//                 [15] => Exemplaires    
-//             */
+        while($row = fgetcsv($fh)){
+            //skip first row
+            if ($num <= 1){ 
+                $num++;
+                continue; 
+            }
+            /*
+                [0] => IdAlbum
+                [1] => Num
+                [2] => Titre
+                [3] => IdSerie
+                [4] => IdDessinateur
+                [5] => IdColoriste
+                [6] => IdScenariste
+                [7] => Editeur
+                [8] => Reference
+                [9] => Couverture
+                [10] =>
+                [11] => Exlibris
+                [12] => NbPages
+                [13] => Planche
+                [14] => IdBEL
+                [15] => Exemplaires    
+            */
 
-//             $row = $this->replaceNull( $row );
+            $row = $this->replaceNull( $row );
 
-//             $book = new Book();
-//             $book->setNum($row[1]);
-//             $book->setTitle($row[2]);
+            $book = new Book();
+            $book->setNumVolume($row[1]);
+            $book->setTitre($row[2]);
 
-//             //get authors
-//             $illustrator = $this->getAuthor( $row[4] );
-//             if ($illustrator)
-//                 $book->setIllustrator( $this->getAuthor( $row[4] ) );
+            //get authors
+            $illustrator = $this->getAuthor( $row[4] );
+            if ($illustrator)
+                $book->setAuthor( $this->getAuthor( $row[4] ) );
+
+            $serie = $this->getSerie( $row[3] );
+            if ($serie)
+                $book->setSerie( $serie );
+
+            $book->setEditeur($row[7]);
+            $book->setReference($row[8]);
+            $book->setCouverture($row[9]);
+            $book->setNbPage($row[12]);
+            $book->setStock($row[15]);
+            $book->setId($row[0]);
+
+            $slug=$this->container->get("cocur_slugify")->slugify($book->getTitre());
+            $book->setSlug($slug);
+
+            $manager->persist( $book );
+        }
+
+        //allow to SET the id, else it will be autogenerated
+        $metadata = $manager->getClassMetaData(get_class($book));
+        $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE); 
+        
+        $manager->flush();
+    }
+
+    private function loadAuthors(ObjectManager $manager){
+
+        $fh = fopen($this->pathToData."authors.csv", "r");
+        $num = 1;
+        while($row = fgetcsv($fh)){
+            //skip first 2 rows
+            if ($num <= 2){ 
+                $num++;
+                continue; 
+            }
+            /*
+                [0] => IdAuteur
+                [1] => Nom
+                [2] => Prenom
+                [3] => Surnom //in fact picture
+                [4] => DateNaissance
+                [5] => DateDeces
+                [6] => Pays
+                [7] => Pseudo //real aka
+                [8] => IdBEL
+            */
+
+            $row = $this->replaceNull($row);
+
+            $author = new Author();
+            $author->setNom($row[1]);
+            $author->setPrenom($row[2]);
+            if ($row[4]){
+                $author->setBirth( $this->getDateTime($row[4]) );
+            }
+            if ($row[5]){
+                $author->setDeath( $this->getDateTime($row[5]) );
+            }
+            $author->setPays( ucfirst(mb_strtolower($row[6], 'UTF-8')) );
+            $author->setPseudo($row[7]);
+
+            $slug=$this->container->get("cocur_slugify")->slugify(($author->getPseudo())." ".($author->getNom())." ".($author->getPrenom()));
+            $author->setSlug($slug);
+
+            $author->setId($row[0]);
+
+            $manager->persist( $author );
+        }
+
+        //allow to SET the id, else it will be autogenerated
+        $metadata = $manager->getClassMetaData(get_class($author));
+        $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE); 
+        
+        $manager->flush();
+    }
+
+
+    private function loadSeries(ObjectManager $manager){
+
+        $fh = fopen($this->pathToData."series.csv", "r");
+        $num = 1;
+        while($row = fgetcsv($fh)){
+            //skip first row
+            if ($num <= 1){ 
+                $num++;
+                continue; 
+            }
+            /*
+            [0] => IdSerie
+            [1] => Titre
+            [2] => Style
+            [3] => Commentaire
+            [4] => NoteGlobale
+            [5] => Planche
+            [6] => IdBEL
+            [7] => Langue
+            */
+
+            $row = $this->replaceNull($row);
+            $serie = new Serie();
             
-//             $colorist = $this->getAuthor( $row[5] );
-//             if ($colorist)
-//                 $book->setColorist( $colorist );
+            $serie->setTitre( $row[1] );
+            $serie->setComment( $row[3] );
 
-//             $scenarist = $this->getAuthor( $row[6] );
-//             if ($scenarist)
-//                 $book->setScenarist( $scenarist );
 
-//             $serie = $this->getSerie( $row[3] );
-//             if ($serie)
-//                 $book->setSerie( $serie );
+            $serie->setId($row[0]);
 
-//             $book->setPublisher($row[7]);
-//             $book->setIsbn($row[8]);
-//             $book->setCover($row[9]);
-//             $book->setExlibris($row[11]);
-//             $book->setPages($row[12]);
-//             $book->setBoard($row[13]);
-//             $book->setIdbel($row[14]);
-//             $book->setStock($row[15]);
+            $manager->persist( $serie );
 
-//             $book->setId($row[0]);
+            $categorie = $serie->getSimpleStyle($row[2]);
 
-//             $book->setDateCreated( new DateTime() );
-//             $book->setDateModified( new DateTime() );
+            $manager->persist($categorie);
+        }
 
-//             $manager->persist( $book );
-//         }
-
-//         //allow to SET the id, else it will be autogenerated
-//         $metadata = $manager->getClassMetaData(get_class($book));
-//         $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE); 
+        //allow to SET the id, else it will be autogenerated
+        $metadata = $manager->getClassMetaData(get_class($serie));
+        $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE); 
         
-//         $manager->flush();
-//     }
-
-//     private function loadAuthors(ObjectManager $manager){
-
-//         $fh = fopen($this->pathToData."authors.csv", "r");
-//         $num = 1;
-//         while($row = fgetcsv($fh)){
-//             //skip first 2 rows
-//             if ($num <= 2){ 
-//                 $num++;
-//                 continue; 
-//             }
-//             /*
-//                 [0] => IdAuteur
-//                 [1] => Nom
-//                 [2] => Prenom
-//                 [3] => Surnom //in fact picture
-//                 [4] => DateNaissance
-//                 [5] => DateDeces
-//                 [6] => Pays
-//                 [7] => Pseudo //real aka
-//                 [8] => IdBEL
-//             */
-
-//             $row = $this->replaceNull($row);
-
-//             $author = new Author();
-//             $author->setLastName($row[1]);
-//             $author->setFirstName($row[2]);
-//             if ($row[4]){
-//                 $author->setBirthDate( $this->getDateTime($row[4]) );
-//             }
-//             if ($row[5]){
-//                 $author->setDeathDate( $this->getDateTime($row[5]) );
-//             }
-//             $author->setCountry( ucfirst(mb_strtolower($row[6], 'UTF-8')) );
-//             $author->setAka($row[7]);
-//             $author->setIdbel($row[8]);
-//             $author->setDateCreated(new DateTime());
-//             $author->setDateModified(new DateTime());
-
-//             $author->setId($row[0]);
-
-//             $manager->persist( $author );
-//         }
-
-//         //allow to SET the id, else it will be autogenerated
-//         $metadata = $manager->getClassMetaData(get_class($author));
-//         $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE); 
-        
-//         $manager->flush();
-//     }
+        $manager->flush();
+    }
 
 
-//     private function loadSeries(ObjectManager $manager){
+    //replace textual NULL values by real null
+    private function replaceNull($row){
+        for($i=0;$i<count($row);$i++){
+            if ($row[$i] == "NULL"){
+                $row[$i] = null;
+            }
+        }
+        return $row;
+    }
 
-//         $fh = fopen($this->pathToData."series.csv", "r");
-//         $num = 1;
-//         while($row = fgetcsv($fh)){
-//             //skip first row
-//             if ($num <= 1){ 
-//                 $num++;
-//                 continue; 
-//             }
-//             /*
-//             [0] => IdSerie
-//             [1] => Titre
-//             [2] => Style
-//             [3] => Commentaire
-//             [4] => NoteGlobale
-//             [5] => Planche
-//             [6] => IdBEL
-//             [7] => Langue
-//             */
-
-//             $row = $this->replaceNull($row);
-//             $serie = new Serie();
-            
-//             $serie->setTitle( $row[1] );
-//             $serie->setStyle( $row[2] );
-//             $serie->setComment( $row[3] );
-//             $serie->setBoard( $row[5] );
-//             $serie->setIdbel( $row[6] );
-
-//             //lang
-//             $serie->setLanguage( $this->getLanguageCode( $row[7] ) );
-
-//             $serie->setDateCreated(new DateTime());
-//             $serie->setDateModified(new DateTime());
-
-//             $serie->setId($row[0]);
-
-//             $manager->persist( $serie );
-//         }
-
-//         //allow to SET the id, else it will be autogenerated
-//         $metadata = $manager->getClassMetaData(get_class($serie));
-//         $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE); 
-        
-//         $manager->flush();
-//     }
+    //get new author from db, else from local property if already set
+    private function getAuthor( $authorId ){
+        if (empty($this->authors[$authorId])){
+            $author = $this->authorRepo->find( $authorId );
+            $this->authors[ $authorId ] = $author;
+        }
+        return $this->authors[ $authorId ];
+    }
 
 
-//     //replace textual NULL values by real null
-//     private function replaceNull($row){
-//         for($i=0;$i<count($row);$i++){
-//             if ($row[$i] == "NULL"){
-//                 $row[$i] = null;
-//             }
-//         }
-//         return $row;
-//     }
+    //get new serie from db, else from local property if already set
+    private function getSerie( $serieId ){
+        if (empty($this->series[$serieId])){
+            $serie = $this->serieRepo->find( $serieId );
+            $this->series[ $serieId ] = $serie;
+        }
+        return $this->series[ $serieId ];
+    }
 
-//     //get new author from db, else from local property if already set
-//     private function getAuthor( $authorId ){
-//         if (empty($this->authors[$authorId])){
-//             $author = $this->authorRepo->find( $authorId );
-//             $this->authors[ $authorId ] = $author;
-//         }
-//         return $this->authors[ $authorId ];
-//     }
+    //return a DateTime object from a shitty date, or false on failure
+    private function getDateTime($date){
+        $twoDigitDate = substr($date, 6, 2);
+        $fourDigitDate = "20" . $twoDigitDate;
+        if ($twoDigitDate > date("y")){
+            $fourDigitDate = "19" . $twoDigitDate;
+        }
+        $date = substr_replace($date, $fourDigitDate, 6, 2);
+        $dateTime = DateTime::createFromFormat("d/m/Y H:i", $date);
+        return $dateTime;
+    }
+
+    //return the language code from full name
+    private function getLanguageCode( $fullName ){
+        $lang = false;
+        switch($fullName){
+            case "Français":
+                $lang = "fr";
+                break;
+            case "Anglais":
+                $lang = "en";
+                break;
+        }
+        return $lang;
+    }
 
 
-//     //get new serie from db, else from local property if already set
-//     private function getSerie( $serieId ){
-//         if (empty($this->series[$serieId])){
-//             $serie = $this->serieRepo->find( $serieId );
-//             $this->series[ $serieId ] = $serie;
-//         }
-//         return $this->series[ $serieId ];
-//     }
-
-//     //return a DateTime object from a shitty date, or false on failure
-//     private function getDateTime($date){
-//         $twoDigitDate = substr($date, 6, 2);
-//         $fourDigitDate = "20" . $twoDigitDate;
-//         if ($twoDigitDate > date("y")){
-//             $fourDigitDate = "19" . $twoDigitDate;
-//         }
-//         $date = substr_replace($date, $fourDigitDate, 6, 2);
-//         $dateTime = DateTime::createFromFormat("d/m/Y H:i", $date);
-//         return $dateTime;
-//     }
-
-//     //return the language code from full name
-//     private function getLanguageCode( $fullName ){
-//         $lang = false;
-//         switch($fullName){
-//             case "Français":
-//                 $lang = "fr";
-//                 break;
-//             case "Anglais":
-//                 $lang = "en";
-//                 break;
-//         }
-//         return $lang;
-//     }
-
-// private function getSimpleStyle($style)
-//     {
-
-//         $default = "Divers";
-
-//         $mappings = array(
-
-//             "Science-fiction" => array(
-//                 "Science-fiction",
-//                 "Heroic Fantasy",
-//                 "Sf/fantastique/anticipation",
-//                 "Science fiction",
-//                 "Anticipation",
-//                 "Fantastique"
-//             ),
-//             "Aventure" => array(
-//                 "Aventure",
-//                 "Aventures",
-//                 "Aventure fantastique"
-//             ),
-//             "Polar/Thriller" => array(
-//                 "Polar/Thriller",
-//                 "Polar",
-//                 "Polar/Thriler",
-//                 "Policier",
-//                 "Thriller"
-//             ),
-//             "Historique" => array(
-//                 "Historique",
-//                 "Histoire",
-//                 "Histoire - Fiction",
-//                 "Aventure historique",
-//                 "Histoire/fiction historique"
-//             ),
-//             "Jeunesse" => array(
-//                 "Jeunesse"
-//             ),
-//             "Humour" => array(
-//                 "Humoristique",
-//                 "Western humoristique",
-//                 "Humour"
-//             ),
-//             "Manga" => array(
-//                 "Manga",
-//                 "Manga - Seinen"
-//             ),
-//             "Biographie" => array(
-//                 "Biographie",
-//                 "Autobiographie",
-//                 "Tranche de vie"
-//             ),
-//             "Indépendant" => array(
-//                 "Independant"
-//             ),
-//             "Érotique" => array(
-//                 "Erotique"
-//             ),
-//             "Comics" => array(
-//                 "Super héros",
-//                 "Comics"
-//             ),
-//             "Divers" => array(
-//                 "Roman graphique",
-//                 "Animalier",
-//                 "Adaptation",
-//                 "Divers",
-//                 "Chronique urbaine",
-//                 "Chronique",
-//                 "Société/quotidien",
-//                 "Drame",
-//                 "NULL",
-//                 NULL,
-//                 false
-//             ),
-//         );
-
-//         //looks for bad name, and returns good one
-//         foreach($mappings as $goodName => $badNames){
-//             if (in_array($style, $badNames)){
-//                 return $goodName;
-//             }
-//         }
-
-//         return $default;
-//     }
-// }
