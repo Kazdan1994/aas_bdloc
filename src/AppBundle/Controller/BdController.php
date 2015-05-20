fc3d9c6364fb441308517949757967d1fd186b64
boulassel <boulasselsamir@gmail.com>
2015-05-20 12:35:20 +0200

    css

--- src/AppBundle/Controller/BdController.php
+++ src/AppBundle/Controller/BdController.php
@@ -7,10 +7,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class BdController extends Controller
 {
      /**
-     * @Route("/liste_bd", name="liste_bd")
+     * @Route("/liste_bd/{page}",requirements={"page":"\d+"}, defaults={"page":1}, name="liste_bd")
      */
-    public function listBdAction()
+    public function listBdAction($page)
     {
-        return $this->render('default/liste_BD.html.twig');
+         
+        
+        //on récupére le repository de Book
+        $storyRepo = $this->get("doctrine")->getRepository("AppBundle:Book");
+        //récupère toutes les BD de la bdd
+        $paginationResults = $storyRepo->findPaginated($page);
+        if (!$paginationResults) {
+            throw $this->createNotFoundException();
+        }
+        //on va passer ces données à twig...
+        $params = array(
+            "paginationResults" => $paginationResults
+        );
+        return $this->render('default/liste_bd.html.twig', $params);
+        
     }
 }
