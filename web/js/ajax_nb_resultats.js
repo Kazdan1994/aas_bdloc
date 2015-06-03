b9ef7904c54645ee6d6667d462d92814a869d9d1
Kazdan1994 <arthurjacquemin2@gmail.com>
2015-06-03 19:45:22 +0200

    Filtres_ajax_select fonctionnel !!!!!!!!!!!!!!!!
    Enfin ça marche, deux semaines que je suis dessus, mais CA MARCHE
    !!!!!!!

--- web/js/ajax_nb_resultats.js
+++ web/js/ajax_nb_resultats.js
@@ -1,82 +1,23 @@
-$(document).ready(function ()
-{
-    $('#bt_filtre2').hide();
+$(document).ready(function () {
 
-    $("#afficher_resultats").on("click", function (e)
-    {
-        /*function xhr ()
-         {
-         var resultats = $("#afficher_resultats").val();
-
-         var request = new XMLHttpRequest();
-         request.onreadystatechange = function () {
-         if (this.readyState == 4) {
-         if (this.status === 200) {
-         console.log(this.responseText);
-         }
-         else {
-         alert(this.responseText);
-         }
-         }
-         };
-
-         request.open('GET', 'liste_bd?afficher_resultats=' + resultats, true);
-         request.send();
-         request = null;
-         }
-         */
-
+    //Lorsqu'on clique sur le select
+    $(".container").on("click", "#afficher_resultats", function () {
+        //On récupère la valeur du select
         var resultats = $("#afficher_resultats").val();
 
-        switch (resultats)
-        {
-            case 'cinq_resultats':
+        var url_select = $(this).attr(resultats); // on crée un url avec le chemin classique + l'id du livre cliqué
 
-                //On ajoute à la requête une limite de 5 résultats
-                $('tbody:nth-child(5)').hide();
-                break;
-
-            case 'dix_resultats':
-
-                //On ajoute à la requête une limite de 10 résultats
-                break;
-
-            case 'vingt_resultats':
-
-                //On ajoute à la requête une limite de 20 résultats
-                break;
-        }
-
-        /*
         $.ajax({
-            type: 'GET',
-            url: 'liste_bd?afficher_resultats=' + resultats,
-            success: function ajaxSuccess(data, textStatus, jqXHR)
+            url: url_select,
+            method: 'GET', // Le type de la requête HTTP
+            data: 'afficher_resultats=' + resultats, //On passe des paramètres avec GET pour les utiliser côté serveur dans l'array $_GET
+            success: function (r) // code_json contient le json renvoyé
             {
-                switch (resultats)
-                {
-                    case 'cinq_resultats':
+                console.log(r); // message response retourné dans le controller;
 
-                        //On ajoute à la requête une limite de 5 résultats
-                        //$('tbody:nth-child(n+5)').hide();
-                        $('tbody:nth-child(n+5)').css('border-color', 'red');
-                        break;
-
-                    case 'dix_resultats':
-
-                        //On ajoute à la requête une limite de 10 résultats
-                        break;
-
-                    case 'vingt_resultats':
-
-                        //On ajoute à la requête une limite de 20 résultats
-                        break;
-                }
-            },
-            error: function ajaxError(jqxhr) {
-                alert(jqxhr.responseText);
+                //On ajoute le html renvoyé par l'AJAX et on met le container bien aligné
+                $('.container').html(r);
             }
-        })
-        */
+        });
     })
 });
